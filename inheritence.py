0902e38b0704f72dbcff85ad51222479f2234d9b
Shikhar Verma <vermashikhar380@gmail.com>
2025-01-27 21:02:30 +0530

    Add files via upload

---
+++ inheritence.py
@@ -0,0 +1,15 @@
+# inheritance
+""
+
+class Myclass(str):
+    def shikhar(self):
+        return 'nothing bye'
+    def sort(self):
+        return sorted(self)    
+
+#main code
+obj = Myclass('hello')
+print(obj.shikhar())
+# print(obj)
+obj.sort()
+
